c35d123974c9817c80490dc289f5f102af4202f1
Chris <resavestation@gmail.com>
2021-05-02 15:43:37 +0800

    1. 發現未把firebase的hoc移除導致出錯

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import LayoutSingle from "@layouts/LayoutSingle";
-import { AuthorizedFirebase } from "@firebase";
+//import { AuthorizedFirebase } from "@firebase";
 import WeatherForecast from "@components/WeatherForecast";
 
 const HomePage = () => {
   return (
-    <AuthorizedFirebase>
-      <LayoutSingle>
-        <WeatherForecast />
-      </LayoutSingle>
-    </AuthorizedFirebase>
+    <LayoutSingle>
+      <WeatherForecast />
+    </LayoutSingle>
   );
 };
 export default HomePage;
